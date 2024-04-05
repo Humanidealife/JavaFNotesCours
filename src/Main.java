c60fb7641cc0901cd6322755d24011fddb6a140d
WANG <wangqianlf@live.com>
2024-04-05 23:03:15 +0200

    types primitifs variable en Java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Deuxième ligne");
+        int age;
+        long nombreDePas = 14527785478789L;
+        int anneeDeNaissance;
+        anneeDeNaissance = 1998;
+        float prixEssence = 1.78F;
+        char prenom = 'Q';
+        prenom = '\uFF27'; // un caractère encodé en format unicode
+        boolean homme = true;
     }
 }
