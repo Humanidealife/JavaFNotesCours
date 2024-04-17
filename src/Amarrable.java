5f7f736bdec9f93e816fc8c4e34ab3c6938a141b
WANG <wangqianlf@live.com>
2024-04-17 15:00:12 +0200

    Interface mot clé implements 3, extends pour Interface et Interface Drapeau, propriété des Interface

--- src/Amarrable.java
+++ src/Amarrable.java
@@ -1,4 +1,4 @@
-public interface Amarrable {
+public interface Amarrable extends Mobile{
 
 
     public int nbCordes(int vitesseDuVent);
--- src/Vidangeable.java
+++ src/Vidangeable.java
@@ -1,4 +1,4 @@
-public interface Vidangeable {
+public interface Vidangeable extends Devissable, Revissable{
     //L'interface va définir cette capacité d'être vidangeable grâce à une ou plusieurs méthodes
     //Ces méthodes vont être déclarées dans l'interface, mais sans en donner le détail, on dit sans en donner l'implémentation
     //On ne dit pas dans l'interface "comment vidanger", parce que cela est propre à chaque classe qui exploite cette interface
