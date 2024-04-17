40cce48c4034a25496fc60cb3680641abf20293d
WANG <wangqianlf@live.com>
2024-04-17 16:15:32 +0200

    mot clé instanceof

--- src/Port.java
+++ src/Port.java
@@ -5,6 +5,14 @@ public class Port {
     //Donc ce que l'on va recevoir en paramètre c'est un objet qui est au moins amarrable
     //C'est-à-dire qui implémente au minimum Amarrable
     void accueilDEngin(Amarrable amarrable){
+
+        //A gauche de "instanceof" on met l'objet que l'on veut tester, en l'occurence amarrable ce qui est passé en paramètre
+        //A droite de "instanceof" on met le type dont on veut savoir si amarrable est issue
+        if (amarrable instanceof Bateau){
+            //Ici ce bloc d'instructions n'a pour l'instant pas de grande importance
+            System.out.println("Il s'agit bien d'un bateau ");
+
+        }
         //On a besoin de connaître comment ça amarre l'engin au Port, donc combien de cordes dont bénéficie
         int nbCordesPort = amarrable.nbCordes(50);
         System.out.println("Le nombre de cordes nécessaires est de " + nbCordesPort);
