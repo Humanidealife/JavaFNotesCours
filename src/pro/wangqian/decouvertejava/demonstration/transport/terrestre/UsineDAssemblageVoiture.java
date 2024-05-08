a6740bd93a4d7f44c170e8c9edbaa6a012f8fd17
WANG <wangqianlf@live.com>
2024-05-08 21:11:51 +0200

    Attraper des exceptions : les mots clé try et catch 1, pas fini

--- src/pro/wangqian/decouvertejava/demonstration/transport/terrestre/UsineDAssemblageVoiture.java
+++ src/pro/wangqian/decouvertejava/demonstration/transport/terrestre/UsineDAssemblageVoiture.java
@@ -7,6 +7,15 @@ public class UsineDAssemblageVoiture extends UsineDAssemblage {
     //  et modifier le type de retour
     public Voiture assembler(){
         Voiture v = new Voiture();
+        //On va essayer d'attribuer une "immatriculation" à notre voiture.
+        //Peu importe que cette immatriculation soit correcte ou pas, notre code comporte désormais une erreur.
+        //Cette erreur nous indique que la méthode "immatriculer" rejette une exception, le type "NombreDeCaractereInvalideException" et que
+        //  l'on devrait à ce niveau-là, nous nous en préoccupons.
+        //Si l'on regarde les propositions de correction, on peut constater deux solutions :
+        //1. On ne souhaite pas se préoccuper de cette exception à ce niveau (le niveau de la méthode "assembler"). On va donc
+        //  rejeter(throws) un peu plus haut
+
+        v.immatriculer("112233");
         return v;
     }
 }
